c9d18e9048cd6ab5be1d2ba91ab9a3fffdb43856
FelipeSantos <live.proto@hotmail.com>
2022-10-09 13:03:33 -0300

    more kubernetes configs and refactoring

---
+++ .dev/commands/kubectl/plugins/dashboard/1-dashboard.install.sh
@@ -0,0 +1,2 @@
+#https://github.com/kubernetes/dashboard
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.7.0/aio/deploy/recommended.yaml
---
+++ .dev/commands/kubectl/plugins/dashboard/2-dashboard.create-user.sh
@@ -0,0 +1,2 @@
+#https://github.com/kubernetes/dashboard/blob/master/docs/user/access-control/creating-sample-user.md
+kubectl apply -f dashboard-adminuser.yml
