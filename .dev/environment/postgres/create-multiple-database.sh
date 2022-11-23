af8955e4c68b4673214d8b4afddfa589786bb797
FelipeSantos <live.proto@hotmail.com>
2022-11-23 15:08:52 -0300

    updated database creation script on docker compose and added api gateway kong/konga

--- .dev/environment/postgres/create-multiple-database.sh
+++ .dev/environment/postgres/create-multiple-database.sh
@@ -3,20 +3,44 @@
 set -e
 set -u
 
-function create_user_and_database() {
-  local database=$1
-  echo "  Creating user and database '$database'"
-  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-      CREATE USER $database;
-      CREATE DATABASE $database;
-      GRANT ALL PRIVILEGES ON DATABASE $database TO $database;
-EOSQL
-}
+psql=( psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" )
 
-if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
-  echo "Multiple database creation requested: $POSTGRES_MULTIPLE_DATABASES"
-  for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
-    create_user_and_database $db
-  done
-  echo "Multiple databases created"
+
+if [ "$POSTGRES_USERS" ]; then
+	USERS_ARR=$(echo $POSTGRES_USERS | tr "|" "\n")
+	for USER in $USERS_ARR
+	do
+		USER_NAME=`echo $USER | cut -d: -f1`
+		USER_PASSWORD=`echo $USER | cut -d: -f2`
+		if [ "$USER_NAME" = 'postgres' ]; then
+			op='ALTER'
+		else
+			op='CREATE'
+		fi
+		"${psql[@]}" <<-EOSQL
+			$op USER "$USER_NAME" WITH SUPERUSER PASSWORD '$USER_PASSWORD' ;
+		EOSQL
+	done
+fi
+
+if [ "$POSTGRES_DATABASES" ]; then
+	DATABASES_ARR=$(echo $POSTGRES_DATABASES | tr "|" "\n")
+	for DATABASE in $DATABASES_ARR
+	do
+		DATABASE_NAME=`echo $DATABASE | cut -d: -f1`
+		DATABASE_OWNER=`echo $DATABASE | cut -d: -f2`
+		if [ "$DATABASE_NAME" != 'postgres' ]; then
+			if [ "$DATABASE_OWNER" ]; then
+				"${psql[@]}" <<-EOSQL
+				CREATE DATABASE "$DATABASE_NAME" owner "$DATABASE_OWNER" ;
+				EOSQL
+				echo
+			else
+				"${psql[@]}" <<-EOSQL
+					CREATE DATABASE "$DATABASE_NAME" ;
+				EOSQL
+				echo
+			fi
+		fi
+	done
 fi
